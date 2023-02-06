c01d2227113fcc572478cd0a71fa6610e34d7c5a
Doyran <m.doyran@uu.nl>
2023-02-06 15:16:18 +0100

    assignment code is finalized

---
+++ assignment.py
@@ -0,0 +1,41 @@
+import glm
+import random
+
+block_size = 1.0
+
+
+def generate_grid(width, depth):
+    # Generates the floor grid locations
+    # You don't need to edit this function
+    data = []
+    for x in range(width):
+        for z in range(depth):
+            data.append([x*block_size - width/2, -block_size, z*block_size - depth/2])
+    return data
+
+
+def set_voxel_positions(width, height, depth):
+    # Generates random voxel locations
+    # TODO: You need to calculate proper voxel arrays instead of random ones.
+    data = []
+    for x in range(width):
+        for y in range(height):
+            for z in range(depth):
+                if random.randint(0, 1000) < 5:
+                    data.append([x*block_size - width/2, y*block_size, z*block_size - depth/2])
+    return data
+
+
+def get_cam_positions():
+    # Generates dummy camera locations at the 4 corners of the room
+    # TODO: You need to input the estimated locations of the 4 cameras in the world coordinates.
+    return [[-64 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, 63 * block_size],
+            [63 * block_size, 64 * block_size, -64 * block_size],
+            [-64 * block_size, 64 * block_size, -64 * block_size]]
+
+
+def get_cam_angles():
+    # Generates dummy camera angles, looking down 45 degrees towards the center of the room
+    # TODO: You need to input the estimated orientations of the 4 cameras in the world coordinates.
+    return [[0, 45, -45], [0, 135, -45], [0, 225, -45], [0, 315, -45]]
